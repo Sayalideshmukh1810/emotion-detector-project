95b299789f259aa8bafda389f5869c7910bc34a7
Sayali <sayalideshmukh1810@gmail.com>
2026-06-17 14:39:56 -0400

    Finalize deployment workspace templates and clear verification metrics

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -2,17 +2,26 @@ import unittest
 from EmotionDetection.emotion_detection import emotion_detector
 
 class TestEmotionDetector(unittest.TestCase):
-    def test_emotion_detector(self):
-        result_1 = emotion_detector("I am glad this happened")
-        self.assertEqual(result_1['dominant_emotion'], 'joy')
-        result_2 = emotion_detector("I am really mad about this")
-        self.assertEqual(result_2['dominant_emotion'], 'anger')
-        result_3 = emotion_detector("I am feel disgusted with this")
-        self.assertEqual(result_3['dominant_emotion'], 'disgust')
-        result_4 = emotion_detector("I am so sad about this")
-        self.assertEqual(result_4['dominant_emotion'], 'sadness')
-        result_5 = emotion_detector("I am really afraid of this")
-        self.assertEqual(result_5['dominant_emotion'], 'fear')
+    
+    def test_joy(self):
+        result = emotion_detector("I am glad this happened")
+        self.assertEqual(result['dominant_emotion'], 'joy')
+        
+    def test_anger(self):
+        result = emotion_detector("I am really mad about this")
+        self.assertEqual(result['dominant_emotion'], 'anger')
+        
+    def test_disgust(self):
+        result = emotion_detector("I am feel disgusted with this")
+        self.assertEqual(result['dominant_emotion'], 'disgust')
+        
+    def test_sadness(self):
+        result = emotion_detector("I am so sad about this")
+        self.assertEqual(result['dominant_emotion'], 'sadness')
+        
+    def test_fear(self):
+        result = emotion_detector("I am really afraid of this")
+        self.assertEqual(result['dominant_emotion'], 'fear')
 
 if __name__ == '__main__':
     unittest.main()
